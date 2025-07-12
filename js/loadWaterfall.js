5487bcf588a06a245e0c8bbcd88e3c11eaaddaa3
Casaer-X <64728283+Casaer-X@users.noreply.github.com>
2025-07-13 04:21:55 +0800

    优化数据获取方式

--- js/loadWaterfall.js
+++ js/loadWaterfall.js
@@ -40,7 +40,7 @@ function loadMoreWaterfall() {
 
 // 页面加载时从后端获取数据
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://data.casarx.top:3000/waterfall')
+    fetch('https://data.casarx.top/waterfall')
         .then(res => res.json())
         .then(data => {
             allItems = data;
